b3347efb7309d1a6b4379c2b32ada50119edd123
sirusbaladi <86076488+sirusbaladi@users.noreply.github.com>
2021-10-29 01:01:50 -0400

    Update schema.sql
    Hey, I proposed some changes. Please give it a look and thank you for doing it as I was preparing for the midterm tomorrow!

--- schema.sql
+++ schema.sql
@@ -1,12 +1,13 @@
-create table Product(
+-- The participation constraints on product and manufacturer cannot be captured in this implementation (keep this comment in the final submittions, only remove the parentheses)
+
+create table Product_produces(
     serial_num char(32) primary key,
     name varchar(128) not null,
     category varchar (32),
-    date_produced date not null, 
-    manufacturuer integer not null unique,
-    brand varchar (128) not null,
+    date_produced date not null, --we don't have this as attribute in our ER, need to be removed)
+    manufacturuer integer not null,
     description varchar(512),
-    foreign key (manufacturuer, brand) references Manufacturuer(mid, name),    
+    foreign key (manufacturuer) references Manufacturuer(mid), --(you only include the primary key of the other entity set, not all attributes). 
 );
 
 create table Manufacturuer(
@@ -15,12 +16,10 @@ create table Manufacturuer(
 );
 
 create table Warehouse(
+    name varchar(128) primary key,
     items_stored integer,
     total_capacity integer
-    street varchar(64),
-    city varchar(64),
-    state varchar(64),
-    zip integer,    
+    address varchar(256) --You added multiple attributes we don't have in our implementation    
 );
 
 create table Seller(
@@ -29,24 +28,21 @@ create table Seller(
 );
 
 create table Customer(
-    customer_id integer primary key,
+    id integer primary key,
     name varchar(64),
     surname varchar(64),
-    street varchar(64),
-    city varchar(64),
-    state varchar(64),
-    zip integer,  
+    address varchar(256) --again, if you want to add multiple attributes, add them in the implementation on the doc too (see slide 50 lec 2)
 );
 
 create table Time(
-    time timestamp primary key
+    time timestamp primary key -- you made it primary key here but not on the ER diagram. 
 );
 
 create table Transaction(
     serial_num char(32),
     customer integer,
     seller integer,
-    timestamp timestamp,
+    timestamp timestamp, --do we need to include this? not sure as it's not a primary key. 
     primary key(serial_num, customer, serial_num, timestamp),
     foreign key (serial_num) references Product(serial_num),
     foreign key (seller) references Seller(sid),
@@ -54,9 +50,9 @@ create table Transaction(
     foreign key (timestamp) references Time(timestamp),
 );
 
-create table Inventory(
+create table Inventory_manage( --this type of names are preferred as make it more clear I believe
     iid integer primary key,
-    manager integer unique not null,
+    manager integer not null, -- only not null is needed for a one sided total constraint
     quantity integer,
     foreign key (manager) references Seller(sid)
 );
@@ -69,11 +65,11 @@ create table Stock(
     foreign key (iid) references Inventory
 );
 
-create table Listing(
+create table List( -- let's keep the name consistent to our ER diag
     serial_num char(32) ,
     sid integer,
     primary key(serial_num, sid),
-    price integer not null,
+    price integer not null, -- debating about this, make sure to change it if you choose to move prime to product
     foreign key (serial_num) references Product(serial_num),
     foreign key (sid) references Seller(sid)    
 );
